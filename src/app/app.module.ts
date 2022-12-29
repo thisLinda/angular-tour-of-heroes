b2b7e09c253c5110fb251722cfafb2e541247aed
thisLinda <la.for.lizzi@gmail.com>
2022-12-29 16:49:32 -0500

    initial commit again

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+// Angular needs to know how the pieces of your application fit together and what other files and libraries the application requires. This information is called metadata. Some of the metadata is in the @Component decorators that you added to your component classes. Other critical metadata is in @NgModule decorators.The most important @NgModule decorator annotates the top-level AppModule class.ng new created an AppModule class in src/app/app.module.ts when it created the project. This is where you opt in to the FormsModule.
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
