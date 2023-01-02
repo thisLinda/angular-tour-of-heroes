240931e6fa9cceea2dfbb8d9067f118e913363a5
thisLinda <la.for.lizzi@gmail.com>
2023-01-02 17:57:23 -0500

    created a form

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,10 +12,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  // The error "Type is not assignable to type 'never'" occurs when we declare an empty array without explicitly typing it and attempt to mutate the array. To solve the error, explicitly type the empty array, e.g. const arr: string[] = [];.https://bobbyhadz.com/blog/typescript-type-is-not-assignable-to-type-never
-  // heroes = Hero[] = [];
-  // heroes: Hero[] = [];
-
   // Add a private heroService parameter of type HeroService to the constructor. The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site. When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
 
   // While you could call getHeroes() in the constructor, that's not the best practice.Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
@@ -28,10 +24,6 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
-
-  // onSelect(hero: Hero): void {
-  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  // }
 
   // The HeroService.getHeroes() method has a synchronous signature, which implies that the HeroService can fetch heroes synchronously. The HeroesComponent consumes the getHeroes() result as if heroes could be fetched synchronously.***This approach won't work in a real application that uses asynchronous calls. It works now because your service synchronously returns mock heroes.***
   // In this tutorial, HeroService.getHeroes() returns an Observable so that it can use the Angular HttpClient.get method to fetch the heroes and have HttpClient.get() return an Observable.
